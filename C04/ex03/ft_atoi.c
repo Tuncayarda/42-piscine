326732e55225b5957f48f4ae7b88dc5ff4aa1a9d
Tuncayarda <160051761+Tuncayarda@users.noreply.github.com>
2024-02-14 20:41:24 +0300

    c04-1

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 17:56:13 by tuaydin           #+#    #+#             */
+/*   Updated: 2024/02/04 15:47:39 by tuaydin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+	{
+		i++;
+	}
+	while (str[i] == 45 || str[i] == 43)
+	{
+		if (str[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		result = (str[i] - 48) + (result * 10);
+		i++;
+	}
+	return (result * sign);
+}
