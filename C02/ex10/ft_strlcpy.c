86eb62d77452ec5726d8436662119d3374144ac3
Tuncayarda <tncyard@gmail.com>
2024-02-19 23:42:14 +0300

    C02-ex10 fix

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:44:59 by tuaydin           #+#    #+#             */
-/*   Updated: 2024/01/31 16:48:20 by tuaydin          ###   ########.fr       */
+/*   Updated: 2024/02/19 23:41:16 by tuaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,14 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 
 	i = 0;
 	len = ft_strlen(src);
-	if (ft_strlen(src) == 0)
-		return (0);
-	while (src[i] != '\0' && i < size - 1)
+	if (size != 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
 	return (len);
 }
