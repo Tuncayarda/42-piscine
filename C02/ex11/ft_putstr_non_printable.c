9eeea5c55732de6a42937c06400fe48b1c9fa116
Tuncayarda <tncyard@gmail.com>
2024-02-19 23:51:43 +0300

    C02-ex11 fix

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 17:29:51 by tuaydin           #+#    #+#             */
-/*   Updated: 2024/01/31 16:50:22 by tuaydin          ###   ########.fr       */
+/*   Updated: 2024/02/19 23:49:53 by tuaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_putstr_non_printable(char *str)
 	hex = "0123456789abcdef";
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 0 && str[i] <= 31)
+		if ((str[i] >= 0 && str[i] <= 31) || str[i] == 127)
 		{
 			ft_putchar('\\');
 			ft_putchar(hex[str[i] / 16]);
